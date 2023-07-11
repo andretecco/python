dcdefc84d8cf7ef2a19475964e4e777e16a1291f
André Tecco <andretecco@gmail.com>
2023-07-10 22:07:48 -0300

    estudos em python - testes com dict

--- escola_v2_com_dict.py
+++ escola_v2_com_dict.py
@@ -13,32 +13,36 @@ __version__ = "0.1.1"
 ##############################################
 
 # Dados
-sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
-sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+alunos = {
+"sala1":["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"],
+"sala2": ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+}
 
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+aulas = {
+"aula_ingles": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+"aula_musica": ["Erik", "Carlos", "Maria"],
+"aula_danca": ["Gustavo", "Sofia", "Joana", "Antonio"]
+}
 
-atividades = [
-    ("Ingles", aula_ingles), 
-    ("Música", aula_musica), 
-    ("Dança", aula_danca),
-]
+atividades = {
+    "Ingles": ["aula_ingles"], 
+    "Música": ["aula_musica"], 
+    "Dança": ["aula_danca"]
+}
 
 # Listar alunos em cada atividade por sala
 
-for nome_atividade, atividade in atividades:
+for nome_atividade, atividade in atividades.items():
 
     print(f"Alunos da atividade {nome_atividade}")
     print("-" * 45)
 
     # Retornar todos os alunos da sala1 que tem interseção com atividade
-    atividade_sala1 = set(sala1) & set(atividade)
-    atividade_sala2 = set(sala2).intersection(atividade)
+    atividades_sala1 = set(alunos["sala1"]) & set(aulas[atividade[0]])
+    atividades_sala2 = set(alunos["sala2"]) & set(aulas[atividade[0]])
 
-    print("Sala1 " , atividade_sala1)
-    print("Sala2 ", atividade_sala2)
+    print("Sala1 " , atividades_sala1)
+    print("Sala2 ", atividades_sala2)
     
     print()
     print("-" * 45)
